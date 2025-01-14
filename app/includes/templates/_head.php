90126ddc3d8664cbfb8b59dd27f4239e30a585ec
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2025-01-14 23:09:00 +0100

    Favicon added

--- app/includes/templates/_head.php
+++ app/includes/templates/_head.php
@@ -20,6 +20,6 @@ function fetchHead()
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-        <link rel="icon" type="image/x-icon" href="favicon.ico">
+        <link rel="icon" type="image/x-icon" href="cowquitaf.ico">
 ';
 }
