e41df66cef63a4c8cd8a4f2f3eb56c5816950c6b
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2025-03-09 22:23:01 +0100

    update npm and build

--- app/includes/templates/_head.php
+++ app/includes/templates/_head.php
@@ -13,8 +13,8 @@ function fetchHead()
         <script type="module" src="http://localhost:5173/js/script.js"></script>
 
         <!-- Production -->
-        <link rel="stylesheet" href="build/assets/script-BjQ0Uw0F.css">
-        <script type="module" src="build/assets/script-BWxrSY78.js"></script>
+        <link rel="stylesheet" href="build/assets/script-Co6dbnCSX.css">
+        <script type="module" src="build/assets/script-DLX95CE22.js"></script>
         <script type="module" src="https://tillyamericanfestival.com/js/script.js"></script>
         
         <!-- AOS -->
