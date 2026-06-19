6c7059ca75628305048415f6b9cd1dc112e949e3
sbonnard <sebastien.bonnard94@gmail.com>
2026-06-19 09:01:07 +0200

    copyright and npm upgrade

--- app/includes/templates/_footer.php
+++ app/includes/templates/_footer.php
@@ -20,6 +20,6 @@ function fetchFooter(array $activeSponsors, string $source = ''): string
             </a>
         </div>
         <p class="footer__credit">Site intégralement réalisé par <br><a class="button button--partner" href="https://sebastien-bonnard-hero.dontrollsingle.fr/" target="_blank">Sébastien Bonnard | Développeur</a></p>
-        <p>©Tilly American Festival 2025</p>
+        <p>©Tilly American Festival 2025-2025</p>
     ';
 }
